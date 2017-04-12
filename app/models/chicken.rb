ab455e5595401361ba478dcefd9bf99cb8768456
Daniel Thompson <danthwa@gmail.com>
2017-04-12 14:50:49 -0700

    have classes in array

--- app/models/chicken.rb
+++ app/models/chicken.rb
@@ -1,15 +1,38 @@
 class Chicken < ApplicationRecord
 
-    CHICKEN_SITE = "http://chickenbreedslist.com/Chicken-Breeds.html"
+    CHICKEN_SITE = "https://en.wikipedia.org/wiki/Chicken_breeds_recognized_by_the_American_Poultry_Association"
+
+    # EGG_COLORS = "http://www.backyardchickens.com/a/egg-color-chart-find-out-what-egg-color-your-breed-lays"
 
     def self.getChickens
-        chickensite = Nokogiri::HTML(open(CHICKEN_SITE))
 
-        links = chickensite.css("a")
-        names = links.css("font")
+        doc = Nokogiri::HTML(open(CHICKEN_SITE))
 
-        names.each do |name|
-            puts name.text.lstrip.rstrip
+        classes = []
+
+        allSpanTags = doc.css('span')
+
+        allSpanTags.each do |span|
+
+            if /Class$/.match(span)
+                classes.push(span)
+            end
+
         end
+
+        classes.each do |chickenClass|
+
+            puts "parent of class"
+            puts chickenClass.parent.next_element
+
+
+        end
+
+        # links = chickensite.css("a")
+        # names = links.css("font")
+        #
+        # names.each do |name|
+        #     puts name.text.lstrip.rstrip
+        # end
     end
 end
